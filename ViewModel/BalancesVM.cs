a6801a9ed4efaea28e55075eb1e50401b6da6218
Joshua Hollenbeck <joshuahollenbeck@outlook.com>
2023-10-05 16:20:23 -0400

    Added placedholder for: Activity History ACH MultiDeposit OrderEntry OrderStatus OrganizeClients Positions

--- ViewModel/BalancesVM.cs
+++ ViewModel/BalancesVM.cs
@@ -9,53 +9,54 @@ namespace Bank.ViewModel
 {
     class BalancesVM : Utilities.ViewModelBase
     {
-        public ObservableCollection<BalanceOverviewModel> BalanceOverviewCollection { get; set; }
+        // public ObservableCollection<BalanceOverviewModel> BalanceOverviewCollection { get; set; }
 
-        public BalanceOverviewModel SelectedCustOverview { get; set; }
+        // public BalanceOverviewModel SelectedCustOverview { get; set; }
 
-        public BalancesVM()
-        {
-            LoadTypes();
-        }
+        // public BalancesVM()
+        // {
+        //     LoadTypes();
+        // }
 
-        private T? GetNullableValue<T>(
-            SqlDataReader reader,
-            string columnName,
-            Func<int, T> getValueFunc
-        )
-            where T : struct
-        {
-            int ordinal = reader.GetOrdinal(columnName);
-            return reader.IsDBNull(ordinal) ? (T?)null : getValueFunc(ordinal);
-        }
+        // private T? GetNullableValue<T>(
+        //     SqlDataReader reader,
+        //     string columnName,
+        //     Func<int, T> getValueFunc
+        // )
+        //     where T : struct
+        // {
+        //     int ordinal = reader.GetOrdinal(columnName);
+        //     return reader.IsDBNull(ordinal) ? (T?)null : getValueFunc(ordinal);
+        // }
 
-        public string GetStringOrNull(SqlDataReader reader, String columnName)
-        {
-            int ordinal = reader.GetOrdinal(columnName);
+        // public string GetStringOrNull(SqlDataReader reader, String columnName)
+        // {
+        //     int ordinal = reader.GetOrdinal(columnName);
 
-            if (!reader.IsDBNull(ordinal))
-            {
-                if (reader.GetFieldType(ordinal) == typeof(string))
-                {
-                    return reader.GetString(ordinal);
-                }
-                else if (reader.GetFieldType(ordinal) == typeof(int))
-                {
-                    return reader.GetInt32(ordinal).ToString();
-                }
-                // ... You can add more type checks if necessary ...
-            }
-            return null;
-        }
+        //     if (!reader.IsDBNull(ordinal))
+        //     {
+        //         if (reader.GetFieldType(ordinal) == typeof(string))
+        //         {
+        //             return reader.GetString(ordinal);
+        //         }
+        //         else if (reader.GetFieldType(ordinal) == typeof(int))
+        //         {
+        //             return reader.GetInt32(ordinal).ToString();
+        //         }
+        //         // ... You can add more type checks if necessary ...
+        //     }
+        //     return null;
+        // }
 
-        public void LoadTypes()
-        {
-            BalanceOverviewCollection = new ObservableCollection<BalanceOverviewModel>();
+        // public void LoadTypes()
+        // {
+        //     BalanceOverviewCollection = new ObservableCollection<BalanceOverviewModel>();
 
-            using (SqlConnection connection = new SqlConnection(Connection.connectionString))
-            {
-                connection.Open();
-                string cust_overview_query =
-        }
+        //     using (SqlConnection connection = new SqlConnection(Connection.connectionString))
+        //     {
+        //         connection.Open();
+        //         // string cust_overview_query =
+        //     }
+        // }
     }
 }
